d505d97e28f9222ad17448867fd98438785cb15e
elizabethnag1311 <109186428+elizabethnag1311@users.noreply.github.com>
2023-01-09 18:09:45 -0300

    auth0App-check

--- src/app/loginComponets/auth-button/auth-button.component.ts
+++ src/app/loginComponets/auth-button/auth-button.component.ts
@@ -2,13 +2,11 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import { DOCUMENT } from '@angular/common';
 
-
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  selector: 'app-auth-button',
+  templateUrl: './auth-button.component.html'
 })
-export class NavbarComponent implements OnInit {
+export class AuthButtonComponent implements OnInit {
 
   constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) { }
 
